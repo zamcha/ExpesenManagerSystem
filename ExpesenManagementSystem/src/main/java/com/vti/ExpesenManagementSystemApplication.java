ae009bc0707a5754525c23b8bef7eb1466f79b2d
zamcha <caotran293@gmail.com>
2025-09-14 18:01:06 +0700

    First commit

---
+++ ExpesenManagementSystem/src/main/java/com/vti/ExpesenManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpesenManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpesenManagementSystemApplication.class, args);
+	}
+
+}
